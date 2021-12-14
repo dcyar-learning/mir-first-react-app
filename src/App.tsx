5b01361a43a8a8b2ecc5a89ea24b537732dd6041
Jhon Achata Limachi <jhonal183@gmail.com>
2021-12-14 06:40:06 -0500

    correcion de tildes

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Mi primera aplicacion</h1>
-      <p>Esta es mi primera aplicacion en React, esta padrisimo!</p>
+      <h1>Mi primera aplicación</h1>
+      <p>Esta es mi primera aplicacion en React, está padrísimo!</p>
     </div>
   );
 }
